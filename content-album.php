62df951498fbf96fd997969cdef142675fce24df
O <chavitak@gmail.com>
2019-07-26 15:00:51 +0900

    album archive 표

--- content-album.php
+++ content-album.php
@@ -41,29 +41,29 @@
 		<?php if ( get_the_content() ) : ?>
 		
 			<div class="post-content">
-			<img src = "<?php the_field('cover');?>"/>
             <table width="100%" border="1" cellspacing="0" cellpadding="0">
-                    <tr>
-                    <td width="25%"><strong>앨범명</strong></td>
-                    <td width="75%"><?php the_field('title'); ?></td>
-                    </tr>
-                    <tr>
-                    <td><strong>아티스트</strong></td>
-                    <td><?php the_field('artist'); ?></td>
-                    </tr>
-                    <tr>
-                    <td><strong>발매일</strong></td>
-                    <td><?php the_field('release_date'); ?></td>
-                    </tr>
-                    <tr>
-                    <td><strong>장르</strong></td>
-                    <td><?php the_field('genre'); ?></td>
-                    </tr>
-                    <tr>
-                    <td><strong>참고점수</strong></td>
-                    <td><?php the_field('rating'); ?></td>
-                    </tr>
-            </table>
+								<tr>
+								<td width="25%"><strong>앨범명</strong></td>
+								<td width="75%"><?php the_field('title'); ?></td>
+								</tr>
+								<tr>
+								<td><strong>아티스트</strong></td>
+								<td><?php the_field('artist'); ?></td>
+								</tr>
+								<tr>
+								<td><strong>발매일</strong></td>
+								<td><?php the_field('release_date'); ?></td>
+								</tr>
+								<tr>
+								<td><strong>장르</strong></td>
+								<td><?php the_field('genre'); ?></td>
+								</tr>
+								<tr>
+								<td><strong>참고점수</strong></td>
+								<td><?php the_field('rating'); ?></td>
+								</tr>
+						</table>
+                                               
 				<?php the_content(); ?>
 			
 			</div><!-- .post-content -->
